723ef9eeb5944bbd5c5aa775cad778d9fc1f4bf4
= <=>
2025-12-05 00:20:55 +0800

    提交一下

---
+++ task_3/service/service.go
@@ -0,0 +1,116 @@
+package service
+
+import (
+	"log"
+	"task3/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var dst = "root:123@Tcp(127.0.0.1:3307)/blog?charset=utf8mb4&parseTime=True&loc=Local"
+var db *gorm.DB
+
+func init() {
+	db, err := gorm.Open(mysql.Open(dst), &gorm.Config{
+		Logger: logger.New(
+			log.Default(),
+			logger.Config{
+				LogLevel: logger.Info,
+				Colorful: true,
+			},
+		),
+	})
+	if err != nil {
+		panic("数据库连接失败")
+	}
+	dbConnections, err := db.DB()
+	if err != nil {
+		panic("数据库连接失败")
+	}
+	dbConnections.SetMaxOpenConns(100)
+	dbConnections.SetMaxIdleConns(10)
+}
+
+type IUserRepository interface {
+	GetById(uint) model.User
+	GetByName(string) model.User
+	GetTags(uint) []model.Tag
+	GetPosts(uint) []model.Post
+	AddOne(*model.User) uint
+}
+
+type IPostRepository interface {
+	AddOne(*model.Post) uint
+	UpdateOne(*model.Post) uint
+	SearchByTags([]model.Tag) []model.Post
+	UpdateStatus(uint8, uint) bool
+	AddComment(comment model.Comment, postid uint) bool
+}
+
+type UserRepository struct{}
+
+func (UserRepository) GetById(id uint) model.User {
+	var user model.User
+	db.First(&user, id)
+	return user
+}
+func (UserRepository) GetByName(name string) model.User {
+	var user model.User
+	db.Where("user_name = ?", name).First(&user)
+	return user
+}
+func (UserRepository) GetTags(id uint) []model.Tag {
+	var tags []model.Tag
+	db.Model(&model.Tag{}).Where("user_id = ?", id).Find(&tags)
+	return tags
+}
+func (UserRepository) GetPosts(id uint) []model.Post {
+	var posts []model.Post
+	db.Model(&model.Post{}).Where("user_id = ?", id).Find(&posts)
+	return posts
+}
+func (UserRepository) AddOne(user *model.User) uint {
+	db.Create(user)
+	return user.ID
+}
+
+type PostRepository struct{}
+
+func (PostRepository) AddOne(post *model.Post) uint {
+	db.Create(post)
+	return post.ID
+}
+func (PostRepository) UpdateOne(post *model.Post) uint {
+	db.Save(post)
+	return post.ID
+}
+
+func (PostRepository) SearchByTags(tags []model.Tag) []model.Post {
+	var posts []model.Post
+	result := db.Model(&model.Post{}).Joins("inner join post_tags as pt on post.id = pt.post_id").Where("pt.tag_id in (?)", tags).Select("post.*").Find(&posts)
+	if result.Error != nil {
+		return nil
+	}
+	return posts
+}
+
+func (PostRepository) UpdateStatus(status uint8, id uint) bool {
+	var post model.Post
+	db.First(&post, id)
+	post.Status = status
+	result := db.Save(&post)
+	return result.Error != nil
+}
+
+func (PostRepository) AddComment(comment model.Comment, postid uint) bool {
+	var post model.Post
+	check := db.First(&post, postid)
+	if check.Error == nil || post.ID == 0 {
+		return false
+	}
+	comment.PostId = postid
+	result := db.Create(&comment)
+	return result.Error != nil
+}
